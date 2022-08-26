76ad00dd5b6910295c6cf2a3d634afcbd84cdcc8
VulSle <sle7@huskers.unl.edu>
2022-08-26 00:25:29 +0000

    initial commit

---
+++ hello2.c
@@ -0,0 +1,16 @@
+/**
+ * Names: Andrew Falk and Stephanie Le
+ * Date: 8/25/22
+ * Hack 1 file that prints name
+ **/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv) {
+
+//Printing the name
+   printf("Andrew and Stephine");
+
+  return 0;
+}
